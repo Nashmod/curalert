f7a4827c69abdd193d5e9d4f2699b35d24f14e5a
Nashmod <n.oleana@profoundprojects.com>
2020-08-28 11:26:36 -0400

    add .env to project ✨

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
+require('dotenv').config();
+
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || '3003';
 
 app.get('/', (req, res) => {
   res.send('hello world');
